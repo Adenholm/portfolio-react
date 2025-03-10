d482f890dab587de4aa75cc00ef0a5029a94d663
Adenholm <hanni-01@live.se>
2025-03-10 22:19:17 +0100

    added gh-pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: { port: 3000 }
+    plugins: [react(), tailwindcss()],
+    server: { 
+        port: 3000 
+    },
+    base: '/portfolio-react/', // Used for deploying to GitHub Pages
 })
